e469b61420f315bea8708e98dc31522157638b27
dni ⚡ <office@dnilabs.com>
2024-01-26 08:02:36 +0100

    fixup wuery helper tests

--- tests/core/test_helpers_query.py
+++ tests/core/test_helpers_query.py
@@ -6,16 +6,16 @@ from lnbits.helpers import (
 )
 from tests.helpers import TestModel
 
-test = TestModel(id=1, name="test")
+test = TestModel(id=1, name="test", value="yes")
 
 
 @pytest.mark.asyncio
 async def test_helpers_insert_query():
     q = insert_query("test_helpers_query", test)
-    assert q == "INSERT INTO test_helpers_query (id, name) VALUES (?, ?)"
+    assert q == "INSERT INTO test_helpers_query (id, name, value) VALUES (?, ?, ?)"
 
 
 @pytest.mark.asyncio
 async def test_helpers_update_query():
     q = update_query("test_helpers_query", test)
-    assert q == "UPDATE test_helpers_query SET id = ?, name = ? WHERE id = ?"
+    assert q == "UPDATE test_helpers_query SET id = ?, name = ?, value = ? WHERE id = ?"
